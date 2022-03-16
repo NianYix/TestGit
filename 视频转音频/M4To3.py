673337d27baf16b263f0e75bc77f1c1254f01b7c
hw_liuhao <liuhao@hellowd.net>
2022-03-16 15:43:34 +0800

    视频转音频

--- 视频转音频/M4To3.py
+++ 视频转音频/M4To3.py
@@ -10,11 +10,11 @@ import entry
 def extract(video_path: str, tmp_dir: str, ext: str):
     file_name = '.'.join(os.path.basename(video_path).split('.')[0:-1])
     print('文件名:{}，提取音频'.format(file_name))
-    return run_ffmpeg(video_path, os.path.join(tmp_dir, '{}.{}'.format(uuid.uuid4(), ext)), ext)
+    return run_ffmpeg(video_path, os.path.join(tmp_dir, r'{}.{}'.format(uuid.uuid4(), ext)), ext)
 
 def run_ffmpeg(video_path: str, audio_path: str, format: str):
-    ff = FFmpeg(inputs={video_path: None},
-                outputs={audio_path: '-f {} -vn'.format(format)})
+    ff = FFmpeg(executable=r'C:\KMPlayer\ffmpeg.exe',inputs={video_path: None},
+                outputs={audio_path: r'-f {} -vn'.format(format)})
     ff.run()
     return audio_path
 
@@ -28,11 +28,11 @@ def get_fileName(fileName):
 def get_file(filePath):
     _filePath=filePath
     print(f'_fileName:{_fileName}-_filePath:{_filePath}')
-    print(extract(_fileName, _filePath))
+    print(extract(_fileName, _filePath,'wav'))
 
 if __name__ == '__main__':
-#     entry.get_file('flv',get_fileName)
-    _fileName=r'F:/Work/JerryStudy/GitPos/TestGit/视频转音频/aa.mp4'
+#     entry.get_file('mp4',get_fileName)
+    _fileName=r'F:/Work/JerryStudy/GitPos/TestGit/视频转音频/mmm.flv'
     _filePath=r'F:/Work/JerryStudy/GitPos/TestGit/视频转音频'
-    print(extract(_fileName, _filePath))
+    print(extract(_fileName, _filePath,'wav'))
     
