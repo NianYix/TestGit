4721627ae8b986ab0b60381f3a837426e8d6a123
hw_liuhao <liuhao@hellowd.net>
2022-04-24 18:25:36 +0800

    图片文字识别

---
+++ 图像文字识别/README.txt
@@ -0,0 +1,6 @@
+1、安装pytesseract报错问题处理：
+	https://www.jianshu.com/p/93ab58dea50f
+2、中文包下载:
+	https://raw.githubusercontent.com/tesseract-ocr/tessdata/master/chi_sim.traineddata
+3、训练：
+	https://blog.csdn.net/a10201516595/article/details/106989088/
---
+++ 图像文字识别/图像文字.py
@@ -0,0 +1,20 @@
+# import pytesseract
+# from PIL import Image
+# # 读取图片
+# im = Image.open(r'F:\Work\JerryStudy\GitPos\TestGit\图像文字识别\ttttttt.jpg')
+# # 识别文字
+# string = pytesseract.image_to_string(im, lang='chi_sim')
+# print(string)
+
+# from cnocr import CnOcr
+# ocr = CnOcr()
+# res = ocr.ocr_for_single_line('sentence.jpg')
+# print("Predicted Chars:", res)
+
+import easyocr
+reader = easyocr.Reader(['ch_sim','en'])
+result = reader.readtext(r'C:\Users\Jerry\Desktop\bx\2021.12.20\sentence.jpg')
+# print(result)
+for i in result:
+    word = i[1]
+    print(word)
